a3dc3f3cd1c7852bb5e5169d45c7219ea822f63f
yeshapatel1000 <37809358+yeshapatel1000@users.noreply.github.com>
2020-08-17 18:17:59 -0700

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,19 @@
+def substringk(s, k):
+    if not s or k == 0:
+        return []
+
+    letter, res = {}, list()
+    start = 0
+    for i in range(len(s)):
+        if s[i] in letter and letter[s[i]] >= start:
+            start = letter[s[i]]+1
+        letter[s[i]] = i
+        # if i-start+1 == k:
+        res.append(s[start:i+1])
+        start += 1
+    return res
+
+
+s = "pqpqs"
+k = 2
+print(substringk(s, k))
